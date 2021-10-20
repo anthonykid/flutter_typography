7da8b9430ad64452051940980a8fa747ff4ea989
AnthonyKid <dickya630@gmail.com>
2021-10-20 23:23:28 +0800

    first commit

---
+++ lib/text_theme.dart
@@ -0,0 +1,29 @@
+    import 'package:flutter/material.dart';
+    import 'package:google_fonts/google_fonts.dart';
+     
+    final myTextTheme = TextTheme(
+      headline1: GoogleFonts.lemonada(
+          fontSize: 82, fontWeight: FontWeight.w300, letterSpacing: -1.5),
+      headline2: GoogleFonts.lemonada(
+          fontSize: 51, fontWeight: FontWeight.w300, letterSpacing: -0.5),
+      headline3: GoogleFonts.lemonada(fontSize: 41, fontWeight: FontWeight.w400),
+      headline4: GoogleFonts.lemonada(
+          fontSize: 29, fontWeight: FontWeight.w400, letterSpacing: 0.25),
+      headline5: GoogleFonts.lemonada(fontSize: 20, fontWeight: FontWeight.w400),
+      headline6: GoogleFonts.lemonada(
+          fontSize: 17, fontWeight: FontWeight.w500, letterSpacing: 0.15),
+      subtitle1: GoogleFonts.lemonada(
+          fontSize: 14, fontWeight: FontWeight.w400, letterSpacing: 0.15),
+      subtitle2: GoogleFonts.lemonada(
+          fontSize: 12, fontWeight: FontWeight.w500, letterSpacing: 0.1),
+      bodyText1: GoogleFonts.sourceSerifPro(
+          fontSize: 18, fontWeight: FontWeight.w400, letterSpacing: 0.5),
+      bodyText2: GoogleFonts.sourceSerifPro(
+          fontSize: 16, fontWeight: FontWeight.w400, letterSpacing: 0.25),
+      button: GoogleFonts.sourceSerifPro(
+          fontSize: 16, fontWeight: FontWeight.w500, letterSpacing: 1.25),
+      caption: GoogleFonts.sourceSerifPro(
+          fontSize: 14, fontWeight: FontWeight.w400, letterSpacing: 0.4),
+      overline: GoogleFonts.sourceSerifPro(
+          fontSize: 11, fontWeight: FontWeight.w400, letterSpacing: 1.5),
+    );
